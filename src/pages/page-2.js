748c477f11e8a18ed7e528c797d0696a9ec4fe87
Siva Kumar <shivjsdev@gmail.com>
2019-12-27 00:37:29 +0100

    [ADDS] a sub page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -10,6 +10,8 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <br/><br/>
+    <Link to="/sub-pages/page-3">Go to the page 3</Link>
   </Layout>
 )
 
