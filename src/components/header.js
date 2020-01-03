d39ce1b0856adce4e3d1648bda5f81e02ccbc521
Siva Kumar <shivjsdev@gmail.com>
2020-01-03 22:59:38 +0100

    [ADDS] netlify identity widget

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types"
 import React from "react"
 import gatsbyIcon from "../images/gatsby-icon.png"
 import { FaShoppingCart } from "react-icons/fa"
+import netlifyIdentity from "netlify-identity-widget"
+import "./header.css"
 
 const NavLink = props => (
   <Link
@@ -12,20 +14,24 @@ const NavLink = props => (
   />
 )
 
+const UserIdentity = () => (
+  <div className="flex self-center flex-col">
+    <div data-netlify-identity-menu></div>
+  </div>
+)
+
 const Logo = ({ icon }) => (
   <img src={icon} className="self-center m-0 w-10 h-10" alt="logo" />
 )
 
 const MyCart = () => {
   return (
-    <div className="snipcart-summary snipcart-checkout flex self-center flex-col ml-auto cursor-pointer bg-purple-900 hover:bg-purple-800 p-2 rounded-lg">
-      <div className="flex flex-row self-end my-2">
-        <span className="snipcart-total-items text-white text-lg text-center font-bold bg-purple-700 rounded-lg px-1">
-          {" "}
-        </span>
-        <FaShoppingCart className="text-white text-lg m-1" />
-        <span className="snipcart-total-price text-white text-lg text-center font-bold bg-purple-700 rounded-lg px-1"></span>
-      </div>
+    <div className="snipcart-summary snipcart-checkout flex self-center cursor-pointer hover:bg-purple-800 p-2 m-1 rounded-lg">
+      <span className="snipcart-total-items text-white text-center bg-red-700 rounded-lg px-1">
+        {" "}
+      </span>
+      <FaShoppingCart className="text-white text-lg mx-2" />
+      <p className="snipcart-total-price text-gray-500 text-sm text-center font-thin"></p>
     </div>
   )
 }
@@ -38,25 +44,40 @@ const Brand = ({ title }) => (
   </h1>
 )
 
-const Header = ({ siteTitle }) => (
-  <header className="bg-purple-700 mb-6">
-    <div className="my-0 mx-auto max-w-6xl pt-6">
-      <div className="flex">
-        <Logo icon={gatsbyIcon} />
-        <Brand title={siteTitle} />
-        <MyCart />
-      </div>
-      <div className="flex">
-        <NavLink to="/page-2"> Page 2 </NavLink>
-        <NavLink to="/posts/page-markdown">Markdowns</NavLink>
-        <NavLink to="/sub-pages/page-images"> Images </NavLink>
-        <div className="m-3 border-l-2 border-purple-800"></div>
-        <NavLink to="/blog"> Blog </NavLink>
-        <NavLink to="/products"> Products </NavLink>
-      </div>
-    </div>
-  </header>
+const RightMenu = () => (
+  <div className="flex flex-row self-end ml-auto">
+    <MyCart />
+    <UserIdentity />
+  </div>
 )
+
+class Header extends React.Component {
+  componentDidMount() {
+    netlifyIdentity.init()
+  }
+  render() {
+    const { siteTitle } = this.props
+    return (
+      <header className="bg-purple-700 mb-6">
+        <div className="my-0 mx-auto max-w-6xl pt-6">
+          <div className="flex">
+            <Logo icon={gatsbyIcon} />
+            <Brand title={siteTitle} />
+            <RightMenu />
+          </div>
+          <div className="flex">
+            <NavLink to="/page-2"> Page 2 </NavLink>
+            <NavLink to="/posts/page-markdown">Markdowns</NavLink>
+            <NavLink to="/sub-pages/page-images"> Images </NavLink>
+            <div className="m-3 border-l-2 border-purple-800"></div>
+            <NavLink to="/blog"> Blog </NavLink>
+            <NavLink to="/products"> Products </NavLink>
+          </div>
+        </div>
+      </header>
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
